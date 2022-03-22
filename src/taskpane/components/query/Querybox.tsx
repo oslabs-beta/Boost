dec44d9da70083ffc7183ab9129e6fbed1ad1ca1
Janson Xavier <jansonxavier15@gmail.com>
2022-03-22 16:08:08 -0400

    starting work on query functionality

--- src/taskpane/components/query/Querybox.tsx
+++ src/taskpane/components/query/Querybox.tsx
@@ -1,9 +1,27 @@
 import React from "react";
 
 export default () => {
-  const onSubmit = () => {
+  const onSubmit = async () => {
     console.log("submit was clicked");
-    // document.getElementById('sqlQueryBox').value;
+    console.log((document.getElementById('sqlQueryBox') as HTMLInputElement).value); 
+
+    await Excel.run(async (context) => {
+      // This method adds a percentage AutoFilter to the active worksheet 
+      // and applies the filter to a column of the used range.
+  
+      // Retrieve the active worksheet and the used range on that worksheet.
+      const sheet = context.workbook.worksheets.getActiveWorksheet();
+      const farmData = sheet.getUsedRange();
+  
+      // Add a filter that will only show the rows with the top 50% of values in column 3.
+      sheet.autoFilter.apply(farmData, 3, {
+          criterion1: "50",
+          filterOn: Excel.FilterOn.topPercent
+      });
+  
+      await context.sync();
+  });
+
   };
 
   return (
