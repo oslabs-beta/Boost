4a399635385540ce9effe4abc5da5b74219c856a
sophiasam96 <sophia.sam96@gmail.com>
2022-03-28 14:25:50 -0400

    global filtering working

--- src/taskpane/components/query/QueryTable.tsx
+++ src/taskpane/components/query/QueryTable.tsx
@@ -1,34 +1,41 @@
 import React from "react";
-import { useTable } from "react-table";
+import { useTable, useGlobalFilter } from "react-table";
+import { GlobalFilter } from "./GlobalFilter";
 
 export default (props: any) => {
   const { columns, data } = props;
 
-  const tableInstance = useTable({
-    columns,
-    data,
-  });
+  const tableInstance: any = useTable(
+    {
+      columns,
+      data,
+    },
+    useGlobalFilter
+  );
 
-  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = tableInstance;
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow, state, setGlobalFilter } = tableInstance;
 
+  const { globalFilter } = state;
   return (
-    <div id="queryTable">
+    <>
+      <GlobalFilter filter={globalFilter} setFilter={setGlobalFilter} />
+
       <table {...getTableProps()}>
         <thead>
-          {headerGroups.map((headerGroup) => (
+          {headerGroups.map((headerGroup: any) => (
             <tr {...headerGroup.getHeaderGroupProps()}>
-              {headerGroup.headers.map((column) => (
+              {headerGroup.headers.map((column: any) => (
                 <th {...column.getHeaderProps()}>{column.render("Header")}</th>
               ))}
             </tr>
           ))}
         </thead>
         <tbody {...getTableBodyProps()}>
-          {rows.map((row) => {
+          {rows.map((row: any) => {
             prepareRow(row);
             return (
               <tr {...row.getRowProps()}>
-                {row.cells.map((cell) => {
+                {row.cells.map((cell: any) => {
                   return <td {...cell.getCellProps()}>{cell.render("Cell")}</td>;
                 })}
               </tr>
@@ -36,6 +43,6 @@ export default (props: any) => {
           })}
         </tbody>
       </table>
-    </div>
+    </>
   );
 };
