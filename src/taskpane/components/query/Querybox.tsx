d53b8cfade0df9540f067de2a71a19abef08fd01
sophiasam96 <sophia.sam96@gmail.com>
2022-03-29 14:10:34 -0400

    SELECT working, memoize :wq

--- src/taskpane/components/query/Querybox.tsx
+++ src/taskpane/components/query/Querybox.tsx
@@ -4,10 +4,9 @@ import { QueryBoxProps } from "src/types";
 /* global JSX */
 
 export default (props: QueryBoxProps): JSX.Element => {
-  const query = "SELECT Population, five, two FROM world_population, Sheet1 WHERE Population > 1000000"
   return (
     <>
-      <textarea id="sqlQueryBox" placeholder="Enter SQL Query">{query}</textarea>
+      <textarea id="sqlQueryBox" placeholder="Enter SQL Query"></textarea>
       <br />
       <button onClick={props.onSubmit}>Submit</button>
     </>
