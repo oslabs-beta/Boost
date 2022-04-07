886a65a7650714c92068a2967b92a82371e7ce4b
sophiasam96 <sophia.sam96@gmail.com>
2022-04-07 18:01:52 -0400

    testing setup, react library setup

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+/* global module */
+module.exports = {
+  preset: "ts-jest/presets/default-esm",
+  globals: {
+    "ts-jest": {
+      useESM: true,
+    },
+    moduleNameMapper: {
+      "^(\\.{1,2}/.*)\\.js$": "$1",
+    },
+  },
+};
